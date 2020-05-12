f8b95c8e7fcdce70b57a66307eb1dcda87ed1aaa
felixseeger <felix@super-code.de>
2020-05-12 13:04:20 +0200

    display inputs fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,16 @@
 function work() {}
 
+/* festlegen der schnittstellen */
 let davor = document.getElementById('davor');
 let hinten = document.getElementById('hinten');
 let vorne = document.getElementById('vorne');
 
+/* einbinden in html mit Prüfung des Inputs und kontitioneler trennung  */
 function trennen() {
 	const zkTrennung = document.getElementById('zk-tp').value;
 	const zkInput = document.getElementById('zk-input').value;
 
+	/* wenn entsprechend gewählt, entsprechend trennen */
 	if (davor.checked) {
 		vorne.innerHTML = zkInput.split(zkTrennung)[0];
 		hinten.innerHTML = zkTrennung + zkInput.split(zkTrennung)[1];
@@ -17,6 +20,7 @@ function trennen() {
 	}
 }
 
+/* zurücksetzten der inputfelder */
 function löschen() {
 	document.getElementById('zk-tp').value = '';
 	document.getElementById('zk-input').value = '';
